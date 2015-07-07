6ab0b8b5cf07f8dc9d282304ebc792e745f9d36a
Peter <petershim61@gmail.com>
2015-07-07 00:00:01 -0700

    made snake move

--- javascript/app.js
+++ javascript/app.js
@@ -1,70 +1,138 @@
-//peter shim
-var canvas;
-var ctx;
-var dx = 5;
-var dy = 5;
-var x = 150;
-var y = 100;
-var WIDTH = 300;
-var HEIGHT = 200;
+var JS_SNAKE = {};
 
-function circle(x,y,r) {
-  ctx.beginPath();
-  ctx.arc(x, y, r, 0, Math.PI*2, true);
-  ctx.fill();
-}
+JS_SNAKE.game = (function () {
+  var ctx;
+  JS_SNAKE.width = 200;
+  JS_SNAKE.height = 200;
+  JS_SNAKE.blockSize = 10;
+  var frameLength = 500; //new frame every 0.5 seconds
+  var snake;
 
-function rect(x,y,w,h) {
-  ctx.beginPath();
-  ctx.rect(x,y,w,h);
-  ctx.closePath();
-  ctx.fill();
-  ctx.stroke();
-}
-
-function clear() {
-  ctx.clearRect(0, 0, WIDTH, HEIGHT);
-}
-
-function init() {
-  canvas = document.getElementById("canvas");
-  ctx = canvas.getContext("2d");
-  return setInterval(draw, 10);
-}
-
-function doKeyDown(evt){
-  switch (evt.keyCode) {
-    case 38:  /* Up arrow was pressed */
-    if (y - dy > 0){
-      y -= dy;
-    }
-    break;
-    case 40:  /* Down arrow was pressed */
-    if (y + dy < HEIGHT){
-      y += dy;
-    }
-    break;
-    case 37:  /* Left arrow was pressed */
-    if (x - dx > 0){
-      x -= dx;
-    }
-    break;
-    case 39:  /* Right arrow was pressed */
-    if (x + dx < WIDTH){
-      x += dx;
-    }
-    break;
+  function init() {
+    $('body').append('<canvas id="jsSnake">');
+    var $canvas = $('#jsSnake');
+    $canvas.attr('width', JS_SNAKE.width);
+    $canvas.attr('height', JS_SNAKE.height);
+    var canvas = $canvas[0];
+    ctx = canvas.getContext('2d');
+    snake = JS_SNAKE.snake();
+    bindEvents();
+    gameLoop();
   }
-}
 
-function draw() {
-  clear();
-  ctx.fillStyle = "white";
-  ctx.strokeStyle = "black";
-  rect(0,0,WIDTH,HEIGHT);
-  ctx.fillStyle = "purple";
-  circle(x, y, 10);
-}
+  function gameLoop() {
+    ctx.clearRect(0, 0, JS_SNAKE.width, JS_SNAKE.height);
+    snake.advance();
+    snake.draw(ctx);
+    setTimeout(gameLoop, frameLength); //do it all again
+  }
 
-window.addEventListener("load", init, false);
-window.addEventListener('keydown',doKeyDown,true);
+  function bindEvents() {
+    var keysToDirections = {
+      37: 'left',
+      38: 'up',
+      39: 'right',
+      40: 'down'
+    };
+
+    $(document).keydown(function (event) {
+      var key = event.which;
+      var direction = keysToDirections[key];
+
+      if (direction) {
+        snake.setDirection(direction);
+        event.preventDefault();
+      }
+      else if (key === 32) {
+        restart();
+      }
+    });
+  }
+
+  return {
+    init: init
+  };
+})();
+
+
+JS_SNAKE.snake = function () {
+  var posArray = [];
+  posArray.push([6, 4]);
+  posArray.push([5, 4]);
+  posArray.push([4, 4]);
+  var direction = 'right';
+  var nextDirection = direction;
+
+  function setDirection(newDirection) {
+    var allowedDirections;
+
+    //If snake is going left or right, only valid new directions
+    //are up and down. Vice versa for up or down.
+    switch (direction) {
+    case 'left':
+    case 'right':
+      allowedDirections = ['up', 'down'];
+      break;
+    case 'up':
+    case 'down':
+      allowedDirections = ['left', 'right'];
+      break;
+    default:
+      throw('Invalid direction');
+    }
+    if (allowedDirections.indexOf(newDirection) > -1) {
+      nextDirection = newDirection;
+    }
+  }
+
+  function drawSection(ctx, position) {
+    var x = JS_SNAKE.blockSize * position[0];
+    var y = JS_SNAKE.blockSize * position[1];
+    ctx.fillRect(x, y, JS_SNAKE.blockSize, JS_SNAKE.blockSize);
+  }
+
+  function draw(ctx) {
+    ctx.save();
+    ctx.fillStyle = '#33a';
+    for(var i = 0; i < posArray.length; i++) {
+      drawSection(ctx, posArray[i]);
+    }
+    ctx.restore();
+  }
+
+  function advance() {
+    var nextPosition = posArray[0].slice();
+    direction = nextDirection;
+    switch (direction) {
+    case 'left':
+      nextPosition[0] -= 1;
+      break;
+    case 'up':
+      nextPosition[1] -= 1;
+      break;
+    case 'right':
+      nextPosition[0] += 1;
+      break;
+    case 'down':
+      nextPosition[1] += 1;
+      break;
+    default:
+      throw('Invalid direction');
+    }
+
+    posArray.unshift(nextPosition);
+    posArray.pop();
+  }
+
+
+  return {
+    draw: draw,
+    advance: advance,
+    setDirection: setDirection
+  };
+};
+
+
+$(document).ready(function () {
+  JS_SNAKE.game.init();
+});
