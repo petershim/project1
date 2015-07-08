e036e427015fb4c4a6a63f4fefb151fa1ff4435e
Peter <petershim61@gmail.com>
2015-07-07 19:31:09 -0700

    2 snakes move, random apple

--- javascript/app.js
+++ javascript/app.js
@@ -7,7 +7,11 @@ JS_SNAKE.game = (function () {
   JS_SNAKE.blockSize = 20;
   var frameLength = 100; //new frame every 0.1 seconds
   var snake;
+  var snake2;
   var apple;
+  var initApple;
+  var x;
+  var y;
 
   function init() {
     $('body').append('<canvas id="jsSnake" style="border:1px solid #000000;">');
@@ -17,25 +21,31 @@ JS_SNAKE.game = (function () {
     var canvas = $canvas[0];
     ctx = canvas.getContext('2d');
     snake = JS_SNAKE.snake();
+    snake2 = JS_SNAKE.snake2();
     apple = JS_SNAKE.apple();
-    bindEvents();
+    initApple = JS_SNAKE.initApple();
+    bindSnakeEvents();
+    bindSnake2Events();
+    initApple.draw(ctx);
     gameLoop();
   }
 
   function gameLoop() {
     ctx.clearRect(0, 0, JS_SNAKE.width, JS_SNAKE.height);
     snake.advance();
+    snake2.advance();
     snake.draw(ctx);
+    snake2.draw(ctx);
     apple.draw(ctx);
     setTimeout(gameLoop, frameLength); //Refresh screen
   }
 
-  function bindEvents() {
+  function bindSnakeEvents() {
     var keysToDirections = {
       37: 'left',
       38: 'up',
       39: 'right',
-      40: 'down'
+      40: 'down',
     };
 
     $(document).keydown(function (event) {
@@ -51,6 +61,27 @@ JS_SNAKE.game = (function () {
       }
     });
   }
+  function bindSnake2Events() {
+    var keysToDirections = {
+      65: 'left',
+      87: 'up',
+      68: 'right',
+      83: 'down',
+    };
+
+    $(document).keydown(function (event) {
+      var key = event.which;
+      var direction = keysToDirections[key];
+
+      if (direction) {
+        snake2.setDirection(direction);
+        event.preventDefault();
+      }
+      else if (key === 32) {
+        restart();
+      }
+    });
+  }
 
   return {
     init: init
@@ -58,15 +89,30 @@ JS_SNAKE.game = (function () {
 })();
 
 JS_SNAKE.apple = function () {
-  var position = [6, 6];
 
   function draw(ctx) {
     ctx.save();
     ctx.fillStyle = '#FF0066'; //apple green
-    var x = position[0] * JS_SNAKE.blockSize;
-    var y = position[1] * JS_SNAKE.blockSize;
     ctx.fillRect(x, y, JS_SNAKE.blockSize, JS_SNAKE.blockSize);
     ctx.restore();
+    console.log(x, y);
+  }
+
+  return {
+    draw: draw
+  };
+};
+
+JS_SNAKE.initApple = function () {
+
+  function draw(ctx) {
+    ctx.save();
+    ctx.fillStyle = '#FF0066'; //apple green
+    x = Math.floor(Math.random()*JS_SNAKE.width);
+    y = Math.floor(Math.random()*JS_SNAKE.height);
+    ctx.fillRect(x, y, JS_SNAKE.blockSize, JS_SNAKE.blockSize);
+    ctx.restore();
+    console.log(x, y);
   }
 
   return {
@@ -152,6 +198,82 @@ JS_SNAKE.snake = function () {
   };
 };
 
+JS_SNAKE.snake2 = function () {
+  var posArray = [];
+  posArray.push([6, 4]);
+  posArray.push([5, 4]);
+  posArray.push([4, 4]);
+  var direction = 'left';
+  var nextDirection = direction;
+
+  function setDirection(newDirection) {
+    var allowedDirections;
+
+    //If snake is going left or right, only valid new directions
+    //are up and down. Vice versa for up or down.
+    switch (direction) {
+    case 'left':
+    case 'right':
+      allowedDirections = ['up', 'down'];
+      break;
+    case 'up':
+    case 'down':
+      allowedDirections = ['left', 'right'];
+      break;
+    default:
+      throw('Invalid direction');
+    }
+    if (allowedDirections.indexOf(newDirection) > -1) {
+      nextDirection = newDirection;
+    }
+  }
+
+  function drawSection(ctx, position) {
+    var x = JS_SNAKE.blockSize * position[0];
+    var y = JS_SNAKE.blockSize * position[1];
+    ctx.fillRect(x, y, JS_SNAKE.blockSize, JS_SNAKE.blockSize);
+  }
+
+  function draw(ctx) {
+    ctx.save();
+    ctx.fillStyle = '#66FFCC';
+    for(var i = 0; i < posArray.length; i++) {
+      drawSection(ctx, posArray[i]);
+    }
+    ctx.restore();
+  }
+  //continuous movement
+  function advance() {
+    var nextPosition = posArray[0].slice();
+    direction = nextDirection;
+    switch (direction) {
+    case 'left':
+      nextPosition[0] -= 1;
+      break;
+    case 'up':
+      nextPosition[1] -= 1;
+      break;
+    case 'right':
+      nextPosition[0] += 1;
+      break;
+    case 'down':
+      nextPosition[1] += 1;
+      break;
+    default:
+      throw('Invalid direction');
+    }
+
+    posArray.unshift(nextPosition);
+    posArray.pop();
+  }
+
+
+  return {
+    draw: draw,
+    advance: advance,
+    setDirection: setDirection
+  };
+};
 
 $(document).ready(function () {
   JS_SNAKE.game.init();
